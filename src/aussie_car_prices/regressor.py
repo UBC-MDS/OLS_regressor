df65e649123dec81aab774ff38bb5e646a254afb
YimengXia <sara.xia9105@gmail.com>
2024-01-12 10:20:55 -0800

    predict function doc done

--- src/aussie_car_prices/regressor.py
+++ src/aussie_car_prices/regressor.py
@@ -10,8 +10,20 @@ class LinearRegressor:
         pass
 
     def predict(self, X):
+        """
+        Predicts target values using the fitted linear model.
+        
+        Parameters
+        ----------
+        X : {array-like matrix} of shape (n_samples, n_features)
+            Feature values that will be used to make predictions.
+        
+        Returns
+        -------
+        predictions : array-like matrix of shape (n_samples, n_targets)
+            Predicted target values for the input feature values.
+        """
         pass
 
     def score(self, X, y):
         pass
-
