0e8da8c11c487a44aca7c3057306e67f072682f0
chenhanx <chenhanx@student.ubc.ca>
2024-01-19 11:07:48 -0800

    merge all changes

--- src/ols_regressor/regressor.py
+++ src/ols_regressor/regressor.py
@@ -93,22 +93,43 @@ class LinearRegressor():
         pred = X @ self.coef
         return pred
 
-    def score(self, y_true, y_pred):
+    def score(self, df, true_column, pred_column):
         """
-        Calculates the coefficient of determination R^2 for the prediction.
+            Calculates the coefficient of determination R^2 for the prediction.
 
-        Parameters
-        ----------
-        y_true : array-like matrix, shape (n_samples, n_targets)
+            Parameters
+            ----------
+            y_true : array-like matrix, shape (n_samples, n_targets)
             True target values.
 
-        y_pred : array-like matrix, shape (n_samples, n_targets)
+            y_pred : array-like matrix, shape (n_samples, n_targets)
             Predicted target values.
 
-        Returns
-        -------
-        r2_score : float
-            Coefficient of determination R^2.
+            Returns
+            -------
+            r2_score : float
+                Coefficient of determination R^2.
         """
-        pass
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("Input must be a pandas DataFrame")
+
+        if df.empty or len(df) < 2:
+            raise ValueError("DataFrame must have at least two data points")
+
+        y_true = df[true_column].values
+        y_pred = df[pred_column].values
+
+        #  Calculate mean value of y_true
+        y_true_mean = np.mean(y_true)
+
+        # Calculat（SST）
+        SST = np.sum((y_true - y_true_mean) ** 2)
+
+        # Calculate（SSE）
+        SSE = np.sum((y_true - y_pred) ** 2)
+
+        # Calculate R^2
+        r2 = 1 - (SSE / SST)
+
+        return r2
 
